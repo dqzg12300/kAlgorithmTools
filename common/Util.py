03309615670a19b3317045c983a3847ffeb8f19b
ajin <aasd1123>
2021-01-20 17:34:50 +0800

    base64提交

--- common/Util.py
+++ common/Util.py
@@ -1,21 +1,130 @@
 import base64
+import re,struct
+
+
+def StrToHexSplit(input):
+    buf = bytes(0)
+    lines = re.split(r'[\r\n ]',input)
+    for code in lines:
+        if len (code) <= 0:
+            continue
+
+        num = int(code,16)
+        bnum = struct.pack('B',num)
+        buf += bnum
+    return buf
 
 def kbase64(input_data,isencode):
     if isencode:
-        res = base64.b64encode(input_data.encode("utf-8"))
+        if type(input_data) is str:
+            buff=input_data.encode("utf-8")
+        elif type(input_data) is bytes:
+            buff=input_data
+        res = base64.b64encode(buff)
         return res.decode("utf-8")
     else:
         try:
-            res = base64.b64decode(input_data.encode("utf-8"))
+            res = base64.b64decode(input_data.encode("utf-8"))  #res是2进制数据 如果不能utf-8显示。我们就直接显示16进制
             outdata=res.decode("utf-8")
-        except:
+        except Exception as ex:
+            if "invalid continuation byte" in str(ex):
+                outdata=""
+                for mych in res:
+                    outdata+="%02x"%mych+" "
+                return outdata
             return ""
         return outdata
 
-def curtomBase64(bkey,input):
-    input_bytes = bytes(input, "utf-8")
-    output = ""
-    cnt = len(input)
+#密钥,加密的数据,编码还是解码,是否显示为16进制
+def newBase64(bkey,input,isencode,ishex):
+    if type(input) is str:
+        buff = input.encode("utf-8")
+    elif type(input) is bytes:
+        buff = input
+    if isencode:
+        res = My_base64_encode(bkey, buff)
+    else:
+        try:
+            res = My_base64_decode(bkey, input)
+            outdata = res.decode("utf-8")
+            return outdata
+        except Exception as ex:
+            if "invalid continuation byte" in str(ex):
+                outdata=""
+                for mych in res:
+                    outdata+="%02x"%mych+" "
+                return outdata
+            return ""
+    if ishex:
+        outdata = ""
+        for mych in res:
+            outdata += "%02x" % mych + " "
+        return outdata
+    return res
+
+
+def My_base64_encode(bkey,inputs):
+    # 将字符串转化为2进制
+    bin_str = []
+    for i in inputs:
+        x = str(bin(i)).replace('0b', '')
+        bin_str.append('{:0>8}'.format(x))
+    # print(bin_str)
+    # 输出的字符串
+    outputs = ""
+    # 不够三倍数，需补齐的次数
+    nums = 0
+    while bin_str:
+        # 每次取三个字符的二进制
+        temp_list = bin_str[:3]
+        if (len(temp_list) != 3):
+            nums = 3 - len(temp_list)
+            while len(temp_list) < 3:
+                temp_list += ['0' * 8]
+        temp_str = "".join(temp_list)
+        # print(temp_str)
+        # 将三个8字节的二进制转换为4个十进制
+        temp_str_list = []
+        for i in range(0, 4):
+            temp_str_list.append(int(temp_str[i * 6:(i + 1) * 6], 2))
+        # print(temp_str_list)
+        if nums:
+            temp_str_list = temp_str_list[0:4 - nums]
+
+        for i in temp_str_list:
+            outputs += bkey[i]
+        bin_str = bin_str[3:]
+    outputs += nums * '='
+    return outputs
+
+
+def My_base64_decode(bkey,inputs):
+    # 将字符串转化为2进制
+    bin_str = []
+    for i in inputs:
+        if i != '=':
+            x = str(bin(bkey.index(i))).replace('0b', '')
+            bin_str.append('{:0>6}'.format(x))
+    # print(bin_str)
+    # 输出的字符串
+    outputs = bytes(0)
+    nums = inputs.count('=')
+    while bin_str:
+        temp_list = bin_str[:4]
+        temp_str = "".join(temp_list)
+        # print(temp_str)
+        # 补足8位字节
+        if (len(temp_str) % 8 != 0):
+            temp_str = temp_str[0:-1 * nums * 2]
+        # 将四个6字节的二进制转换为三个字符
+        for i in range(0, int(len(temp_str) / 8)):
+            outputs += struct.pack("B",int(temp_str[i * 8:(i + 1) * 8], 2))
+        bin_str = bin_str[4:]
+    return outputs
+
+def curtomBase64(bkey,input_bytes):
+    output = bytes(0)
+    cnt = len(input_bytes)
     precnt = cnt - 2
     cnt1 = cnt
     v6 = 0
--- kwindow/kmainForm.py
+++ kwindow/kmainForm.py
@@ -1,5 +1,6 @@
+import datetime
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 
 from common import Util
 from kwindow.kmain import Ui_MainWindow
@@ -12,16 +13,49 @@ class kmainForm(QMainWindow,Ui_MainWindow):
 
     def base64_calc(self):
         base64_input = self.txtbase64_input.toPlainText()
-        res=Util.kbase64(base64_input,self.rdobase64_encode.isChecked())
+        res=Util.newBase64(self.txtbase64_key.text(),base64_input,self.rdobase64_encode.isChecked(),self.chkIsHex.isChecked())
+        # res=Util.kbase64(base64_input,self.rdobase64_encode.isChecked())
         self.txtbase64_output.setPlainText(res)
 
     def base64_input_change(self):
         base64_input = self.txtbase64_input.toPlainText()
-        res = Util.kbase64(base64_input, self.rdobase64_encode.isChecked())
+        res = Util.newBase64(self.txtbase64_key.text(),base64_input, self.rdobase64_encode.isChecked(),self.chkIsHex.isChecked())
+        # res = Util.kbase64(base64_input, self.rdobase64_encode.isChecked())
         self.txtbase64_output.setPlainText(res)
+
+    def select_file(self):
+        filename= QFileDialog.getOpenFileName()[0]
+        if len(filename)<=0:
+            return
+        self.txtbase64file.setText(filename)
+        with open(filename,"rb") as myfile:
+            mydata= myfile.read()
+            res = Util.newBase64(self.txtbase64_key.text(),mydata, self.rdobase64_encode.isChecked(),self.chkIsHex.isChecked())
+            self.txtbase64_output.setPlainText(res)
+
+    def save_file(self):
+        output=self.txtbase64_output.toPlainText()
+        if len(output)<=2:
+            self.appendLog("错误的输出结果.无法保存")
+            return
+        savefile= QFileDialog.getSaveFileName()[0]
+        if len(savefile)<=0:
+            self.appendLog("取消选择文件")
+            return
+
+        res=Util.StrToHexSplit(output)
+        #开始保存
+        with open(savefile,"wb") as myfile:
+            myfile.write(res)
+            self.appendLog("保存成功到:"+savefile)
+
+    def appendLog(self,logstr):
+        datestr = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S   ')
+        self.txtLog.appendPlainText(datestr+logstr)
 
 if __name__=="__main__":
     app=QApplication(sys.argv)
     kmain = kmainForm()
     kmain.show()
     sys.exit(app.exec_())
+
